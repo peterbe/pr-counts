4f838140ede02bbba6b03006482b16302b4f98ca
Your Name <you@example.com>
2026-02-08 08:40:11 -0500

    debug

--- packages/scraper/drizzle.config.ts
+++ packages/scraper/drizzle.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "drizzle-kit";
 import { DATABASE_URL } from "./src/db";
 
+console.log("HERE IN DRIZZLE CONFIG", { DATABASE_URL });
 export default defineConfig({
 	dialect: "postgresql",
 	schema: "./src/schema.ts",
--- packages/scraper/src/db.ts
+++ packages/scraper/src/db.ts
@@ -3,4 +3,6 @@ import { drizzle } from "drizzle-orm/node-postgres";
 export const DATABASE_URL =
 	process.env.DATABASE_URL || "postgres://localhost:5432/pr-counts";
 
+console.log("HERE IN DB.ts", { DATABASE_URL });
+
 export const db = drizzle(DATABASE_URL);
